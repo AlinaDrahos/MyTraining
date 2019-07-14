c3480d449ff26b6441344d6a7d131600ee8b9064
alinc <alinc@DESKTOP-G7RINBJ>
2019-07-14 18:20:39 -0500

    More Changes

---
+++ Exercise15/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise15
+{
+    class Program
+    {
+        static string word;
+        //static int index1;
+        //static int index2;
+        //static int index3;
+
+        static void Main(string[] args)
+        {
+            word = args[0];
+            //index1 = int.Parse(args[1]);
+            //index2 = int.Parse(args[2]);
+            //index3 = int.Parse(args[3]);
+
+            //Method(index1);
+            //Method(index2);
+            //Method(index3);
+            for (int i = 1; i < args.Length; i++)
+            {
+                int index = Convert.ToInt32(args[i]);
+                Method(index);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Method(int myIndex)
+        {
+            string newString=null;
+            char[] myword = word.ToCharArray();
+            for (int i = 0; i < myword.Length; i++)
+            {
+                if (i != myIndex)
+                {
+                    newString += myword[i];
+                }
+            }
+            Console.WriteLine(newString);
+        }
+    }
+}
---
+++ Exercise16/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                string word = args[i];
+                Method(word);
+            }
+        }
+
+        public static void Method(string myWord)
+        {
+            string newWord = null;
+            string firstLetter = null;
+            string lastLetter = null;
+            for (int i = 0; i < myWord.Length; i++)
+            {
+                firstLetter = myWord[0].ToString();
+                lastLetter = myWord[(myWord.Length) - 1].ToString();
+                if (i!=0 && i!=(myWord.Length-1) )
+                {
+                    newWord += myWord[i];
+                }
+            }
+
+            newWord = lastLetter + newWord + firstLetter;
+            Console.WriteLine(newWord);
+            Console.ReadLine();
+        }
+    }
+}
